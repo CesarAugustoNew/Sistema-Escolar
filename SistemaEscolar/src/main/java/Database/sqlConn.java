2fa23dcc797fbac734d7029b9d7edfef3ce1b4df
CesarAugustoNew <cesarprofissional22@gmail.com>
2026-05-06 21:55:31 -0300

    Iniciando projeto Sistema Escolar

---
+++ SistemaEscolar/src/main/java/Database/sqlConn.java
@@ -0,0 +1,28 @@
+package Database;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class sqlConn {
+    private static final String url = "jdbc:mysql://localhost:3306/sistema_escolar";
+    private static final String user = "root";
+    private static final String password = "Senai@134";
+
+    public static Connection getConnection() throws SQLException
+    {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void testConnection() {
+        try (Connection conn = getConnection()) {
+            System.out.println("Conexão foi bem estabelecida " + conn);
+        } catch (SQLException e) {
+            System.out.println("Falha na conexão " + e.getMessage());
+            System.out.println("Verifique: ");
+            System.out.println("1. MySql está rodando ?");
+            System.out.println("2. O banco " + url + "realmente existe ?");
+            System.out.println("3. O Usuario ou senha estão corretos ?");
+        }
+    }
+}
---
+++ SistemaEscolar/src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import Database.sqlConn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        sqlConn.testConnection();
+    }
+}
